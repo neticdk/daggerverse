094e4d8cadcecefc68c930f572bac2b5d30158f0
Christian Bøgh Larsen [Netic] <cla@netic.dk>
2025-08-05 09:52:21 +0200

    fix: get dagger version

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 .PHONY: test
 test: ## Run unit tests for each module
 	@hack/do.sh test
+
+.PHONY: dagger-version
+dagger-version: ## Get the latest dagger version used
+	@hack/do.sh dagger_version
--- hack/do.sh
+++ hack/do.sh
@@ -3,7 +3,6 @@
 set -eu
 
 test() {
-    pwd
     find ./ -type f -name "test.sh" -print0 | while IFS= read -r -d '' test_script; do
         (
             dir=$(dirname "$test_script")
@@ -11,6 +10,54 @@ test() {
             cd "$dir" && bash ./"$(basename "$test_script")"
         )
     done
+}
+
+dagger_version() {
+    local version="0.0.0"
+    while IFS= read -r -d '' test_script; do
+        local dir
+        dir=$(dirname "$test_script")
+        if [ -f "$dir/dagger.json" ]; then
+            v=$(yq '.engineVersion' "$dir/dagger.json" | sed 's/"//g' | sed 's/v//')
+            if [[ -z "$v" || "$v" == "null" ]]; then
+                continue
+            fi
+            set +e
+            vercomp "$version" "$v"
+            ret=$?
+            set -e
+            if [ $ret -eq 2 ]; then
+                version="$v"
+            fi
+        fi
+    done < <(find ./ -type f -name "test.sh" -print0)
+    echo "$version"
+}
+
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if ((10#${ver1[i]:=0} > 10#${ver2[i]:=0}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
 }
 
 "$@"
